1ce3215b877eec0acf9d8ade84a115440e36087b
andrey-gabchak <andrey.gabchak@gmail.com>
2018-09-01 16:20:41 +0300

    Task 'Rangers'

--- src/academy/mate/homeworks/hw1/Main.java
+++ src/academy/mate/homeworks/hw1/Main.java
@@ -1,26 +1,33 @@
 package academy.mate.homeworks.hw1;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Choose task");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter array length: ");
         int arrayLength = scanner.nextInt();
         System.out.println("Enter 'k' step: ");
         int k = scanner.nextInt();
 
+        //TODO:choosing task
+
         int[] array = createRandomArray(arrayLength);
 
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(arrayRotation(array, k)));
 
-        int [] arrayMono = {1, 2, -1, 4, 7, 6, -2, 4}; //TODO: delete the shitcode
+        int[] arrayMono = {1, 2, -1, 4, 7, 6, -2, 4}; //TODO: delete the shitcode
         System.out.println(Arrays.toString(restoreArray(arrayMono)));
 
+        int[] arrayRange = {1, 2, 3, 5, 8, 9, 10, 13, 14, 15, 16};
+        ArrayList<int[]> range = ranges(arrayRange);
+        for (int[] ints : range) {
+            System.out.print(Arrays.toString(ints));
+        }
     }
+
 
     private static int[] arrayRotation(int[] array, int k) {
         int[] result = new int[array.length];
@@ -43,6 +50,28 @@ public class Main {
         for (int i = 1; i < result.length - 1; i++) { //Negative elements (NE) can be ONLY INSIDE monotone part. So NE can't be first and last position.
             if (result[i] < 0) {
                 result[i] = result[i - 1] / 2 + result[i + 1] / 2;
+            }
+        }
+        return result;
+    }
+
+    private static ArrayList<int []> ranges(int [] inputArray) {
+        ArrayList<int[]> result = new ArrayList<>();
+
+        for (int i = 0; i < inputArray.length - 1; i++) {
+            int [] tempArray = new int[2];
+            if (inputArray[i + 1] - inputArray[i] != 1) {
+                result.add(new int[]{inputArray[i]});
+            } else {
+                tempArray[0] = inputArray[i];
+                for (int j = i; j < inputArray.length - 1; j++) {
+                    if (inputArray[i + 1] - inputArray[i] != 1) {
+                        break;
+                    }
+                    tempArray[1] = inputArray[j];
+                    i = j;
+                }
+                result.add(tempArray);
             }
         }
         return result;
