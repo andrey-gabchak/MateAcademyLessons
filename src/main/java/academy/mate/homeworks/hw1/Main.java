44c7c5c6867c623a25e6a70b33e9f706b5df053a
andrey-gabchak <andrey.gabchak@gmail.com>
2018-09-09 22:13:18 +0300

    Remove System.arraycopy(). Used 'for' cycle.

--- src/main/java/academy/mate/homeworks/hw1/Main.java
+++ src/main/java/academy/mate/homeworks/hw1/Main.java
@@ -43,14 +43,18 @@ public class Main {
 
     private static int[] arrayRotation(int[] array, int k) {
         int[] result = new int[array.length];
-        System.arraycopy(array, k, result, 0, array.length - k);
-        System.arraycopy(array, 0, result, array.length - k, k);
+        for (int i = k, j = 0; i < array.length; i++, j++) {
+            result[j] = array[i];
+        }
+        for (int i = 0, j = k + 1; i < k; i++, j++) {
+            result[j] = array[i];
+        }
         return result;
     }
 
     private static int[] restoreArray(int[] inputArray) {
         int[] result = inputArray;
-        for (int i = 1; i < result.length - 1; i++) { //Negative elements (NE) can be ONLY INSIDE monotone part. So NE can't be first and last position.
+        for (int i = 1; i < result.length - 1; i++) {
             if (result[i] < 0) {
                 result[i] = (result[i - 1] + result[i + 1]) / 2;
             }
@@ -58,6 +62,8 @@ public class Main {
         return result;
     }
 
+    //TODO: without ArrayList
+    //TODO: without second for
     private static ArrayList<int[]> ranges(int[] inputArray) {
         ArrayList<int[]> result = new ArrayList<>();
 
