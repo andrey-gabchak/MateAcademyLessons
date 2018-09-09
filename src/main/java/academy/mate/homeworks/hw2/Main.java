195fe08a29f29c977c99ad071caec7189361d192
andrey-gabchak <andrey.gabchak@gmail.com>
2018-09-10 00:34:57 +0300

    Comparision of string in the method 'isPalindrome' without methods of String

--- src/main/java/academy/mate/homeworks/hw2/Main.java
+++ src/main/java/academy/mate/homeworks/hw2/Main.java
@@ -6,18 +6,27 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter repeating character: ");
         char inputChar = scanner.next().charAt(0);
+        System.out.print("Enter input string: ");
         String inputString = scanner.next();
         Main main = new Main();
-        System.out.println(main.isPalindrome(inputString) ? "Is Main" : "Isn't palindrome");
-        System.out.println(main.stringPartReverse(inputString, 2, 4));
-        System.out.println(main.frequencyOfChar(inputChar, inputString));
-        //Do I need write input part 'Choose the task' like hw1?
+        System.out.println(main.isPalindrome(inputString) ? "Is Palindrome" : "Isn't palindrome");
+        System.out.println("Reverse string is '" + main.stringPartReverse(inputString, 2, 4) + "'");
+        System.out.println("Character " + inputChar + " repeat " + main.frequencyOfChar(inputChar, inputString) + " times");
     }
 
     private boolean isPalindrome(String inputString) {
         String reverse = stringPartReverse(inputString, 0, inputString.length() - 1);
-        return reverse.equalsIgnoreCase(inputString);
+        char[] inputChars = inputString.toCharArray();
+        char[] reverseChars = reverse.toCharArray();
+        boolean palindrome = true;
+        for (int i = 0; i < inputChars.length; i++) {
+            if (inputChars[i] != reverseChars[i]) {
+                palindrome = false;
+            }
+        }
+        return palindrome;
     }
 
     private String stringPartReverse(String inputString, int indexFrom, int indexTo) {
